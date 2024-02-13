ac9c8c5105c6a9fc6c287a37d4c31ad9a1a84f23
SimrannJariyal <157213118+SimrannJariyal@users.noreply.github.com>
2024-02-13 21:28:20 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,11 +3,11 @@
 session_start();
 
 // check if the user is already logged in
-if(isset($_SESSION['username']))
-{
-    header("location: welcome.php");
-    exit;
-}
+// if(isset($_SESSION['username']))
+// {
+//     header("location: PROJECT1/trial1.html");
+//     exit;
+// }
 require_once "config.php";
 
 $username = $password = "";
@@ -50,89 +50,134 @@ if(empty($err))
                             $_SESSION["loggedin"] = true;
 
                             //Redirect user to welcome page
-                            header("location: welcome.php");
+                            // echo 'done';
+                            header("location: PROJECT1/trial1.html");
                             
                         }
+                     
                     }
-
+                } else {
+                    $err = "Invalid username or password";
                 }
-
+            } else {
+                $err = "Something went wrong. Please try again later.";
+            }
+        }
     }
-}    
+    ?>
+               
 
-
-}
-
-
-?>
-
-<!doctype html>
+               <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>its_login page</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            background-color: rgb(215, 215, 221);
+            display: flex;
+            height: 100vh;
+            justify-content: center;
+            align-items: center;
+        }
+        .container{
+            background-color: white;
+            height: 80vh;
+            width: 60%;
+            border-radius: 10px;
+            display: flex;
+            flex-direction: row-reverse;
+        }
+        .right-box{
+            height: 80vh;
+            width: 50%;
+          padding: 10%;
+          
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+           
+        }
+        .left-box{
+            height: 80vh;
+            width: 50%;
+            background-color: blue;
+           
+            border-radius: 10px;
+            border-top-right-radius: 70%;
+            border-bottom-right-radius: 70%; /* Push to the right */
+            color: white;
+            display: flex;
+           align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            
+        }
+        a{
+            color: red;
+        }
+       
+        h1, p .right-box{
+           
+             /* Reset default margins */
+            padding: 0;
+            margin: 0;/* Adjust the multiplier as needed */
+        }
+        @media (max-width:730px) {
+            h1, p{
+            font-size: 2vw;
+        }
+        }
+        .left-box h1{
+            /* background-color: rgb(215, 208, 208); */
+        }
+        input{
+            background-color: rgb(218, 237, 237);
+            border-radius: 4px;
+        }
+        button{
+            background-color: aqua;
+            color: white;
+           
+        }
+        button:hover{
+            background-color: chartreuse;
+            cursor: pointer;
+        }
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="right-box">
+     <h1>SIGN IN</h1>
+     <form action="" method="POST">
+        <p>Username *:</p>
+        <input placeholder="Enter email id" type="email" name="username" required>
+        <p>Password *:</p>
+        <input placeholder="Enter Password" type="text" name="password" required>
+      <br><br>
+        <button>Submit</button>
+        <?php if (!empty($err)) { echo "<p style='color: red;'>$err</p>"; } ?>
+     </form>
+        </div>
 
-    <title>PHP login system!</title>
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Php Login System</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-  <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="register.php">Register</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
-      </li>
+        <div class="left-box">
+          
+<h1>Welcome Back!!!</h1>
 
+<p> Login Here</p>
+<br><br>
+<p>Not Registered?? Then </p>
+    <a href="register.php">Register here</a>
+    
+        </div>
       
-     
-    </ul>
-  </div>
-</nav>
 
-<div class="container mt-4">
-<h3>Please Login Here:</h3>
-<hr>
-
-<form action="" method="post">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-
-
-</div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    </div>
+    
+</body>
 </html>
