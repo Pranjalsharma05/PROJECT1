af0b2f0e87c67c03f786c9ecbcff5d25cbffd989
SimrannJariyal <157213118+SimrannJariyal@users.noreply.github.com>
2024-02-22 23:47:34 +0530

    Add files via upload
    email added

---
+++ email1.py
@@ -0,0 +1,36 @@
+
+import smtplib
+import sys
+
+def send_email(sender, recipient, subject, body):
+    message = f"Subject: {subject}\n\n{body}"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, "")
+    server.sendmail(sender, recipient, message)
+    server.quit()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    subject = "Thank you for your registration"
+    body = "Thanking for your registration,This notification is sent by Hospital_management system to "+username
+    send_email('ujbaljariyal@gmail.com', username, subject, body)
+
+
+
+
+
+
+
+
+
+# import smtplib
+
+# server = smtplib.SMTP("smtp.gmail.com", 587)
+# server.starttls()
+
+# server.login('ujbaljariyal@gmail.com',"tgbhscqnbxrtvljt")
+
+# server.sendmail('ujbaljariyal@gmail.com','pranjal202522@gmail.com',"mail from python to ps")
+
+# print("Email has been sent to")
